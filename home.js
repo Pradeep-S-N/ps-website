dd2556600d8b6c4b4008f998aa8c107951d31124
Pradeep-S-N <singhk91061@gmail.com>
2025-03-21 15:38:29 -0700

    u

---
+++ home.js
@@ -0,0 +1,11 @@
+// Example: Add functionality to the "Get Started" button in the header
+const getStartedHeaderBtn = document.getElementById("getStartedHeaderBtn");
+
+if (getStartedHeaderBtn) {
+  getStartedHeaderBtn.addEventListener("click", () => {
+    alert("Header Get Started button clicked!");
+  });
+}
+
+// You can add more JavaScript to handle form submissions, 
+// open/close modals, toggle menus, etc.
